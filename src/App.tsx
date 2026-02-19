3abfd3ac4a1cf1e59b73cc08eb9e50e91104f309
Brandon James <bjames105@outlook.com>
2026-02-19 10:57:06 -0500

    Added shadui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { Button } from './components/ui/button'
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from './components/ui/card'
+import { ModeToggle } from './components/mode-toggle'
+import { ThemeProvider } from "./components/theme-provider"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,26 +12,34 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel='noopener'>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel='noopener'>
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider storageKey="vite-ui-theme">
+        <ModeToggle />
+        <div className="flex justify-center">
+          <a href="https://vite.dev" target="_blank" rel='noopener'>
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank" rel='noopener'>
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <div className="flex justify-center">
+          <Card className="w-96">
+            <CardHeader>
+              <CardTitle className="text-2xl">Vite + React</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </Button>
+            </CardContent>
+            <CardFooter>
+              <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+              </p>
+            </CardFooter>
+          </Card>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
