76da9851c79aa0c246fdb3ba32d17226af8ca700
Brandon James <bjames105@outlook.com>
2026-03-25 18:14:37 -0400

    Changed build action to navigate to the cloned directory before running build commands

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => ({
-  base: mode == "production" ? "/web-media-manager/" : "/",
+  base: mode === "production" ? "/web-media-manager/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
